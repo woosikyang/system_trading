cafc654172d5b0fdcb45d447d095652a5873e2f7
woosikyang <woosik1505@korea.ac.kr>
2021-05-31 23:46:24 +0900

    mariadb setting

--- mariadb.py
+++ mariadb.py
@@ -7,10 +7,18 @@ conn = pymysql.connect(host=configs.ip,
                        user='root',
                        password=configs.password,
                        charset='utf8',
+                       database='TEST',
                        port=3306)
 
 cur = conn.cursor()
 
 sql = "create table if not exists userTable (id char(4), userName char(10))"
 
+#sql 실행
 cur.execute(sql)
+
+#저장
+conn.commit()
+
+#종료
+conn.close()
